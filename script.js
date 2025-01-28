609864542c50501228cd2cd84d8e6d554846a18b
Ubongar <114475363+Ubongar@users.noreply.github.com>
2025-01-28 14:29:21 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: [" Blinding Lights", " Starboy (featuring Daft Punk)", " Save Your Tears", " The Hills", " Can't Feel My Face"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
